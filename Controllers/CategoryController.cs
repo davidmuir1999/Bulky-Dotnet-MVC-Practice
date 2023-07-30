0547db829efa880c026d79a8e8e7b9d04d60e8c5
davidmuir1999 <david.bme.muir2017@gmail.com>
2023-07-30 22:59:12 +0100

    Edit and Delete Categories

--- Controllers/CategoryController.cs
+++ Controllers/CategoryController.cs
@@ -26,7 +26,7 @@ namespace BulkyWebMVCProject.Controllers
         [HttpPost]
         public IActionResult Create(Category obj) //when creating a post method object category is needed
         {
-            if(obj.Name == obj.DisplayOrder.ToString()) // this is server side validation
+            if (obj.Name == obj.DisplayOrder.ToString()) // this is server side validation
             {
                 ModelState.AddModelError("name", "The Display Order cannot exactly match the Name.");
             }
@@ -35,10 +35,71 @@ namespace BulkyWebMVCProject.Controllers
                 _db.Categories.Add(obj); // telling entity framework you have to add this category object to category table
                 _db.SaveChanges();//go to database and create that category
                 return RedirectToAction("Index");
-            //reason we redirect to index is because index and category are in same controller
-            //if they were in different controllers, you would write category, not index
+                //reason we redirect to index is because index and category are in same controller
+                //if they were in different controllers, you would write category, not index
             }
             return View();
+        }
+
+        public IActionResult Edit(int? id)
+        {
+            if (id == null || id == 0)
+            {
+                return NotFound();
+            }
+            Category? categoryFromDb = _db.Categories.Find(id);  //retrieving category from db
+            //Other methods to retrieve
+            //Category? categoryFromDb = _db.Categories.FirstOrDefault(u=>u.Id==id);
+            //Category? categoryFromDb = _db.Categories.Where(u=>.Id==id).FirstOrDefault;
+            if (categoryFromDb == null)
+            {
+                return NotFound();
+            }
+            return View(categoryFromDb);
+        }
+        [HttpPost]
+        public IActionResult Edit(Category obj) //when creating a post method object category is needed
+        {
+            if (ModelState.IsValid)// validation that will add new category to database only if validations are met
+            {
+                _db.Categories.Update(obj); // telling entity framework you have to update this category object to category table
+                _db.SaveChanges();//go to database and create that category
+                return RedirectToAction("Index");
+                //reason we redirect to index is because index and category are in same controller
+                //if they were in different controllers, you would write category, not index
+            }
+            return View();
+        }
+
+        public IActionResult Delete(int? id)
+        {
+            if (id == null || id == 0)
+            {
+                return NotFound();
+            }
+            Category? categoryFromDb = _db.Categories.Find(id);  //retrieving category from db
+            //Other methods to retrieve
+            //Category? categoryFromDb = _db.Categories.FirstOrDefault(u=>u.Id==id);
+            //Category? categoryFromDb = _db.Categories.Where(u=>.Id==id).FirstOrDefault;
+            if (categoryFromDb == null)
+            {
+                return NotFound();
+            }
+            return View(categoryFromDb);
+        }
+        [HttpPost, ActionName("Delete")]
+        public IActionResult DeletePOST(int? id) //when creating a post method object category is needed
+        {
+            Category? obj = _db.Categories.Find(id);
+            if (obj == null)
+            {
+                return NotFound();
+            }
+            _db.Categories.Remove(obj);//remove category obj with given id
+            _db.SaveChanges();//go to database and create that category
+            return RedirectToAction("Index");
+            //reason we redirect to index is because index and category are in same controller
+            //if they were in different controllers, you would write category, not index
         }
     }
 }
